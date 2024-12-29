0471e14eb61d60c22cad502dd5e4cb19c659a062
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-29 19:07:16 +0100

    feat: :sparkles: index.js add check if github caniemail copied

---
+++ index.js
@@ -0,0 +1,32 @@
+import simpleGit from "simple-git";
+import fs from "fs";
+
+const git = simpleGit();
+
+const checkIfRepoExists = async () => {
+  try {
+    const dirPath = "./caniemail";
+
+    if (fs.existsSync(dirPath)) {
+      // Change the current directory to "caniemail"
+      process.chdir("./caniemail");
+
+      // Check if the current directory is a Git repository
+      const isRepo = await git.checkIsRepo();
+      if (isRepo) {
+        return true;
+      } else {
+        console.log("Das Verzeichnis `caniemail` ist kein Git-Repository.");
+        return false;
+      }
+    } else {
+      console.log("Das Verzeichnis `caniemail` existiert nicht.");
+      return false;
+    }
+  } catch (error) {
+    console.error("Ein Fehler ist aufgetreten:", error);
+    return false;
+  }
+};
+
+console.log("checkIfRepoExists", await checkIfRepoExists());
