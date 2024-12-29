4363319403da0be5cafc54e0d6abeafeed1d84ce
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-29 23:05:36 +0100

    refactor: :recycle: remove console.log

--- update.service.js
+++ update.service.js
@@ -12,8 +12,6 @@ export class UpdateService extends EventEmitter {
         const nicenames = await this.fetchNicenames();
         const features = await this.fetchFeatures();
 
-        console.log(apiVersion, nicenames, features);
-
         if (apiVersion && nicenames && features) {
             const apiResponse = {
                 api_version: apiVersion,
