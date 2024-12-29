c3727f29d194ef5d7f7bb89100fa2feee0047ac3
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-29 21:40:06 +0100

    feat: :sparkles: Simplify checkIfRepoExists

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-import simpleGit from 'simple-git';
 import fs from 'fs';
 import { exec } from '@actions/exec';
 import { UpdateService } from './update.service.js';
 
-const git = simpleGit();
-
-const checkIfRepoExists = async () => {
-    try {
-        const dirPath = './caniemail';
-
-        if (fs.existsSync(dirPath)) {
-            // Change the current directory to "caniemail"
-            process.chdir('./caniemail');
-
-            // Check if the current directory is a Git repository
-            const isRepo = await git.checkIsRepo();
-            if (isRepo) {
-                return true;
-            } else {
-                console.error(
-                    'Das Verzeichnis `caniemail` ist kein Git-Repository.',
-                );
-                return false;
-            }
-        } else {
-            console.error('Das Verzeichnis `caniemail` existiert nicht.');
-            return false;
-        }
-    } catch (error) {
-        console.error('Ein Fehler ist aufgetreten:', error);
-        return false;
-    }
-};
-
-if (!checkIfRepoExists()) {
+if (!fs.existsSync('./caniemail/.git')) {
     console.error('Das Verzeichnis `caniemail` existiert nicht.');
     process.exit(1);
 }
